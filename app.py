1d925eb7333e38789cfd57cdb46379b25c1b7625
Diego A. Arturo A. <diegoarturoangulo30@gmail.com>
2024-11-25 01:07:05 -0500

    opciones

--- app.py
+++ app.py
@@ -3,23 +3,116 @@ import streamlit as st
 def main():
     st.title("Gestión de Maquinas")
 
-    # Contenedor principal
+    # Maquina1
     with st.container(border=True):
         # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
         with st.container():
             st.header("Maquina1", divider="gray")
             
-            # st.markdown('''
-            #             Esta maquina es tiene no se que y no que mas
-            #             Es muy importante porque si no se quema todo 
-            #             ''')
-        # Subcontenedor para el enlace
         with st.container():
-            st.write("**Acceso informacion :**")
+            # st.write("**Acceso informacion :**")
             with st.container(border=True):
                 st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
 
+    #maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina2", divider="gray")
             
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina3
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina3", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina4", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina5", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina6", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina7", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina8", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina9", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
+#maquina2
+    with st.container(border=True):
+        # st.write("Bienvenido al gestor de mantenimiento. Aquí puedes acceder a diferentes recursos.")
+        with st.container():
+            st.header("Maquina10", divider="gray")
+            
+        with st.container():
+            # st.write("**Acceso informacion :**")
+            with st.container(border=True):
+                st.page_link("pages/maquina1.py", label='Ir a recursos', use_container_width=True)
+
 
     
             
